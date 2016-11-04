a788545ea4aa67fa47ce295136fdd3e929a8db04
Niko Fink <nfink95@gmail.com>
2016-11-03 21:14:42 -0400

    fix optional comparative values for reference data #2

--- drive4data/data/trips.py
+++ drive4data/data/trips.py
@@ -109,14 +109,14 @@ def preprocess_trips(client):
     detector = TripDetection(time_epoch=client.time_epoch)
     res = client.stream_series(
         "samples",
-        fields="time, veh_speed, participant, veh_odometer, hvbatt_soc, outside_air_temp",
+        fields="time, veh_speed, participant, veh_odometer, hvbatt_soc, outside_air_temp, fuel_rate, hvbatt_current",
         batch_size=500000,
         where="veh_speed > 0")
     for nr, (series, iter) in enumerate(res):
         logger.info(__("#{}: {}", nr, series))
         cycles_curr, cycles_curr_disc = detector(progress(iter))
 
-        logger.info(__("Writing {} + {} = {} cycles", len(cycles_curr), len(cycles_curr_disc),
+        logger.info(__("Writing {} + {} = {} trips", len(cycles_curr), len(cycles_curr_disc),
                        len(cycles_curr) + len(cycles_curr_disc)))
         client.write_points(
             detector.cycles_to_timeseries(cycles_curr + cycles_curr_disc, "trips"),
