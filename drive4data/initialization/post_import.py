1b4af1f3ce1f97f26a683d3929473dc37c1583f0
Niko Fink <nfink95@gmail.com>
2016-11-05 13:50:51 -0400

    adapted post-import script to new tags without car_id

--- drive4data/initialization/post_import.py
+++ drive4data/initialization/post_import.py
@@ -12,7 +12,6 @@ __author__ = "Niko Fink"
 logger = logging.getLogger(__name__)
 
 SAVE_FILE = "tmp/counts.pickle"
-OUT_FILE = "out/counts.csv"
 TIME_EPOCH = 's'
 FIELDNAMES = ['time', 'key', 'first', 'last', 'duration', 'min_soc', 'max_soc', 'count_ac_hvpower',
               'count_boardtemperature', 'count_charger_accurrent', 'count_charger_acvoltage', 'count_chargerplugstatus',
@@ -22,7 +21,7 @@ FIELDNAMES = ['time', 'key', 'first', 'last', 'duration', 'min_soc', 'max_soc', 
               'count_hvbatt_voltage', 'count_hvbs_cors_crnt', 'count_hvbs_fn_crnt', 'count_inputvoltage',
               'count_ischarging', 'count_maf', 'count_motorvoltages', 'count_outside_air_temp', 'count_reltime',
               'count_source', 'count_veh_odometer', 'count_veh_speed', 'count_vin_1', 'count_vin_2', 'count_vin_3',
-              'count_vin_digit', 'count_vin_frame1', 'count_vin_frame2', 'count_vin_index']
+              'count_vin_digit', 'count_vin_frame1', 'count_vin_frame2', 'count_vin_index', 'count_car_id']
 
 
 def extract_res(res, data, func):
@@ -38,32 +37,34 @@ def extract_res(res, data, func):
 
 def analyze(cred):
     if os.path.isfile(SAVE_FILE):
-        data = pickle.load(open(SAVE_FILE, "rb"))
+        with open(SAVE_FILE, "rb") as f:
+            data = pickle.load(f)
     else:
         data = {}
         with contextlib.closing(InfluxDBClient(cred['host'], cred['port'], cred['user'], cred['passwd'], cred['db'],
                                                time_epoch=TIME_EPOCH)) as client:
             logger.info(__("Querying res_first"))
             res_first = client.query(
-                "SELECT participant, car_id, first(source) FROM samples GROUP BY participant, car_id")
+                "SELECT participant, first(source) FROM samples GROUP BY participant")
             extract_res(res_first, data, lambda row: ('first', row['time']))
 
             logger.info(__("Querying res_last"))
             res_last = client.query(
-                "SELECT participant, car_id, last(source) FROM samples GROUP BY participant, car_id")
+                "SELECT participant, last(source) FROM samples GROUP BY participant")
             extract_res(res_last, data, lambda row: ('last', row['time']))
 
             logger.info(__("Querying res_count"))
-            res_count = client.query("SELECT count(*) FROM samples GROUP BY participant, car_id")
+            res_count = client.query("SELECT count(*) FROM samples GROUP BY participant")
             extract_res(res_count, data, lambda row: ('counts', row))
 
             logger.info(__("Querying res_range"))
             res_range = client.query("SELECT min(hvbatt_soc) AS min_soc, max(hvbatt_soc) AS max_soc "
-                                     "FROM samples WHERE hvbatt_soc < 200 GROUP BY participant, car_id")
+                                     "FROM samples WHERE hvbatt_soc < 200 GROUP BY participant")
             extract_res(res_range, data, lambda row: ('min_soc', row['min_soc']))
             extract_res(res_range, data, lambda row: ('max_soc', row['max_soc']))
 
-        pickle.dump(data, open(SAVE_FILE, "wb"))
+        with open(SAVE_FILE, "wb+") as f:
+            pickle.dump(data, f)
 
     return ({
                 'key': k,
